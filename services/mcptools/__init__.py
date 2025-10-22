a9aad3b2bd32ab611a1374e4b8842fa8ec74da0f
anjingzyj <lucifermzhang@163.com>
2025-10-22 09:16:02 +0800

    新增天气查询功能，修改系统提示词

--- services/mcptools/__init__.py
+++ services/mcptools/__init__.py
@@ -1,7 +1,7 @@
 from .base import ToolBase
 from .music_player import MusicPlayerTool
 from .math_tools_demo import MathTool
-
+from .get_weather import WeatherTool
 # 注册工具
 def get_tools() -> list[ToolBase]:
-    return [MusicPlayerTool(), MathTool()]
+    return [MusicPlayerTool(), MathTool(), WeatherTool()]
--- services/mcptools/math_tools_demo.py
+++ services/mcptools/math_tools_demo.py
@@ -13,27 +13,10 @@ class MathTool(ToolBase):
         super().__init__(name='数学计算器')
 
     def usage(self):
-        return '''1.用户输入：涉及加法运算时，使用add_numbers工具
-2.用户输入：涉及减法运算时，使用subtract_numbers工具  
-3.用户输入：涉及乘法运算时，使用multiply_numbers工具
-4.用户输入：涉及复杂数学表达式时，使用evaluate_expression工具'''
-    
-    @tool(description='如果运算符为#，使用该函数对两个数操作 two numbers,减法操作，返回第一个数减去第二个数的结果')
-    def sub(a: int = Field(..., description="被减数"), b: int = Field(..., description="减数")) -> int:
-        logger.info("The sub method is called: a=%d, b=%d", a, b)
-        return a - b
+        return '''1.涉及数学表达式计算时，使用evaluate_expression工具
+根据工具调用返回的结果综合回复，比如工具返回计算结果，回复用户yyy的计算结果为xxx'''
 
-    @tool(description='如果运算符为@，使用该函数对两个数操作 two numbers,乘法操作，返回第一个数乘以第二个数的结果')
-    def mul(a: int = Field(..., description="被乘数"), b: int = Field(..., description="乘数")) -> int:
-        logger.info("The mul method is called: a=%d, b=%d", a, b)
-        return a * b
-
-    @tool(description='如果运算符为&，使用该函数对两个数操作 two numbers,加法操作，返回第一个数加上第二个数的结果')
-    def add(a: int = Field(..., description="被加数"), b: int = Field(..., description="加数")) -> int:
-        logger.info("The add method is called: a=%d, b=%d", a, b)
-        return a + b
-
-    @tool(description='计算给定的算式，符合标准的数学表达式')
+    @tool(description='计算给定的算式，符合标准的数学表达式，需要将用户输入转换为标准python字符串数学表达式格式')
     def any_express(s: str = Field(..., description="待计算的数学表达式")) -> float:
         """计算给定的算式，符合标准的数学表达式"""
         logger.info("The eval method is called: s=%s", s)
