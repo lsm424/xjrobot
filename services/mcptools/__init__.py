dc6b3321c3b588e4510a5278794ff092654aea1d
anjingzyj <lucifermzhang@163.com>
2025-10-23 16:19:10 +0800

    新增预回复，修复bug

--- services/mcptools/__init__.py
+++ services/mcptools/__init__.py
@@ -3,6 +3,8 @@ from .music_player import MusicPlayerTool
 from .math_tools_demo import MathTool
 from .get_weather import WeatherTool
 from .news_search import NewsSearchTool
+from .pre_reply import PreReplyTool  # 添加预回复工具导入
+
 # 注册工具
 def get_tools() -> list[ToolBase]:
-    return [MusicPlayerTool(), MathTool(), WeatherTool(), NewsSearchTool()]
+    return [MusicPlayerTool(), MathTool(), WeatherTool(), NewsSearchTool(), PreReplyTool()]  # 添加预回复工具
